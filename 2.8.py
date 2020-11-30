b4eb9354fae0973b9db915d2eea7f2a862a1e97e
cwilkins-8 <71436174+cwilkins-8@users.noreply.github.com>
2020-11-30 22:36:56 +0000

    Add files via upload

---
+++ 2.8.py
@@ -0,0 +1,11 @@
+#2.8 Number 8. This shows multications showing the number 8
+print (5 + 3)
+print (9 - 1)
+print (2 * 4)
+print (16 / 2)
+# 2.9 Favourite Number: This shows my favourite number as a variable
+
+fave_num = "8"
+message = "my favourite number is.."
+print (message + fave_num)
+#This is a comment
